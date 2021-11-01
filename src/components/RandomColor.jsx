a2ea537fda4b554bf99b60cbae681a50f2a4056c
Triana Cerda <trianaRcerda@gmail.com>
2021-11-01 15:21:27 -0700

    set up state and added onRandomChangeFunction

--- src/components/RandomColor.jsx
+++ src/components/RandomColor.jsx
@@ -4,13 +4,26 @@ class RandomColor extends Component {
   state = {
     displayedColor: 'tan',
   };
+
+  onRandonFunction = (color) => {
+    this.setState({ backgroundColor: color });
+  };
+
   render() {
+    const color = [
+      'black',
+      'purple',
+      'yellow',
+      'teal',
+      // 'https://i.dailymail.co.uk/i/pix/2016/03/18/15/324D202500000578-3498922-image-a-33_1458315465874.jpg',
+    ];
+    const { displayedColor } = this.state;
     return (
       <>
         <h1>RandomColorLab</h1>
         <div
           style={{
-            backgroundColor: this.state.displayedColor,
+            backgroundColor: displayedColor,
             height: 240,
             width: 240,
             marginTop: 8,
