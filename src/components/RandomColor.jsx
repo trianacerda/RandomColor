e9e1a505d338833636e5d1639a6cd957c1ce39c0
Triana Cerda <trianaRcerda@gmail.com>
2021-11-01 16:13:19 -0700

    moved display of color to component

--- src/components/RandomColor.jsx
+++ src/components/RandomColor.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import DisplayChange from './containers/DisplayChange';
 
 class RandomColor extends Component {
   state = {
@@ -6,10 +7,7 @@ class RandomColor extends Component {
   };
 
   getRandomColor() {
-    const color =
-      this.state.displayedColor[
-        Math.floor(Math.random() * this.state.colorList.length)
-      ];
+    this.state.displayedColor[Math.floor(Math.random() * colorList.length)];
     this.setState({ backgroundColor: displayedColor });
   }
 
@@ -25,19 +23,7 @@ class RandomColor extends Component {
       'teal',
       // 'https://i.dailymail.co.uk/i/pix/2016/03/18/15/324D202500000578-3498922-image-a-33_1458315465874.jpg',
     ];
-    return (
-      <>
-        <h1>Random-Color-Lab</h1>
-        <div
-          style={{
-            backgroundColor: displayedColor,
-            height: 810,
-            width: 810,
-            marginTop: 24,
-          }}
-        ></div>
-      </>
-    );
+    return <DisplayChange />;
   }
 }
 
--- src/components/containers/DisplayChange.jsx
+++ src/components/containers/DisplayChange.jsx
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
 
 class DisplayChange extends React.Component {
+  state = {
+    displayedColor: 'tan',
+  };
   render() {
-    return <div></div>;
+    return (
+      <>
+        <h1>Random-Color-Lab</h1>
+        <div
+          style={{
+            backgroundColor: this.state.displayedColor,
+            height: 500,
+            width: 500,
+            marginTop: 24,
+          }}
+        ></div>
+      </>
+    );
   }
 }
 
