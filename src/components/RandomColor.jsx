8710a8a4f11c86b48f69586cb09d1b5b920c222a
Triana Cerda <trianaRcerda@gmail.com>
2021-11-01 17:03:21 -0700

    adjusted comp and moved things around + render to page with multiple colors and baby

--- src/components/RandomColor.jsx
+++ src/components/RandomColor.jsx
@@ -1,29 +1,62 @@
 import React, { Component } from 'react';
-import DisplayChange from './containers/DisplayChange';
-
 class RandomColor extends Component {
   state = {
     displayedColor: 'tan',
-  };
-
-  getRandomColor() {
-    this.state.displayedColor[Math.floor(Math.random() * colorList.length)];
-    this.setState({ backgroundColor: displayedColor });
-  }
-
-  // onRandomColorChange = (color) => {
-  //   this.setState({ backgroundColor: color });
-  // };
-
-  render() {
-    const colorList = [
+    colorList: [
       'black',
       'purple',
-      'yellow',
+      'darkorange',
+      'gold',
+      'deeppink',
+      'red',
+      'cornsilk',
+      'darkgreen',
+      'brown',
+      'darkblue',
+      'chocolate',
+      'salmon',
+      'aquamarine',
       'teal',
-      // 'https://i.dailymail.co.uk/i/pix/2016/03/18/15/324D202500000578-3498922-image-a-33_1458315465874.jpg',
-    ];
-    return <DisplayChange />;
+      'darkgoldenrod',
+      'darkmagenta',
+      'darkviolet',
+      'url("https://i.dailymail.co.uk/i/pix/2016/03/18/15/324D202500000578-3498922-image-a-33_1458315465874.jpg")',
+    ],
+  };
+
+  handleColorChange = () => {
+    setInterval(() => {
+      const randomColor = this.getRandomColor();
+      this.setState({ displayedColor: randomColor });
+    }, 1000);
+  };
+
+  componentDidMount() {
+    this.handleColorChange();
+  }
+
+  getRandomColor() {
+    const color =
+      this.state.colorList[
+        Math.floor(Math.random() * this.state.colorList.length)
+      ];
+    return color;
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Random-Color-Lab</h1>
+        <div
+          style={{
+            background: this.state.displayedColor,
+            height: 500,
+            width: 500,
+            marginTop: 24,
+          }}
+        ></div>
+      </>
+    );
   }
 }
 
